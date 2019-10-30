ca510c3a69ea71e9dfaa6a428697a490459d88ec
NigeldeJ <57156709+NigeldeJ@users.noreply.github.com>
2019-10-30 09:56:47 +0100

    Update

--- js/java.js
+++ js/java.js
@@ -1,12 +1,10 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
@@ -22,7 +20,7 @@ function showSlides(n) {
         slideIndex = slides.length
     }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display= "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
